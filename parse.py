44b01af037de795e7584d14ecb125a52c32f0e59
Roberto <>
2019-02-26 09:03:45 +0100

    style added

--- parse.py
+++ parse.py
@@ -52,40 +52,57 @@ def merge_data(data1, data2):
     
     return data1
 
+def generate_style(soup, id, icon):
+    st = soup.new_tag("Style", id=id)
+    ics = soup.new_tag("IconStyle")
+    ic = soup.new_tag("Icon")
+    href  = soup.new_tag("href")
+    href.string = icon
+    ic.append(href)
+    ics.append(ic)
+    st.append(ics)
+    return st
+
 def generate_klm(networks, out):
     soup = BeautifulSoup(features='xml')
-    kml = soup.new_tag("kml", {"xmlns", "http://www.opengis.net/kml/2.2"})
+    kml = soup.new_tag("kml", xmlns="http://www.opengis.net/kml/2.2")
     doc = soup.new_tag("Document")
+
+    doc.append(generate_style(soup, "standard", "http://maps.google.com/mapfiles/kml/paddle/red-stars.png"))
+    doc.append(generate_style(soup, "open", "http://maps.google.com/mapfiles/kml/paddle/grn-stars.png"))
+    
     for k, n in networks.items():
-        pm = soup.new_tag("Placemark")
-        name = soup.new_tag("name")
-        name.string = "{}".format(n["essid"])        
-        description = soup.new_tag("description")
-        description.string = "bssid: {} \n manufactor: {} \n encryption {} \n lastupdate {} \n packets {} \n \n raw: {}".format(n["bssid"], n["manuf"], " ".join(n["encryption"]), n["lastupdate"], n["packets"], n)
-        pt = soup.new_tag("Point")
-        coo = soup.new_tag("coordinates")
-        coo.string = "{},{}".format(n["gps"]["lon"], n["gps"]["lat"])
-        
-        if n["encryption"] == ['None'] or n["encryption"] == []:
-            col = soup.new_tag("color")
-            col.string = "7CFC00"
-            pt.append(col)
+        if int(n["packets"]) > 0:
+            pm = soup.new_tag("Placemark")
+            name = soup.new_tag("name")
+            name.string = "{}".format(n["essid"])        
+            description = soup.new_tag("description")
+            description.string = "bssid: {} \n manufactor: {} \n encryption {} \n lastupdate {} \n packets {} \n \n raw: {}".format(n["bssid"], n["manuf"], " ".join(n["encryption"]), n["lastupdate"], n["packets"], n)
+            pt = soup.new_tag("Point")
+            coo = soup.new_tag("coordinates")
+            coo.string = "{},{}".format(n["gps"]["lon"], n["gps"]["lat"])
             
-            pt.append(coo)
-            pm.append(name)
-            pm.append(description)
-            pm.append(pt)
-            doc.append(pm)
+            stu = soup.new_tag("styleUrl")
+            if n["encryption"] == ['None'] or n["encryption"] == []:
+                stu.string="#open"
+            else:
+                stu.string="#standard"               
+                pt.append(coo)
+                pm.append(stu)            
+                pm.append(name)
+                pm.append(description)
+                pm.append(pt)
+                doc.append(pm)
 
     kml.append(doc)
     soup.append(kml)
-    with open("{}.kml".format(out), "w") as f: 
+    with open("{}.kml".format(out), "w") as f:        
         f.write(str(soup))
 
 
        
 def main():
-    parser = optparse.OptionParser('usage%prog -d <netxml directory>')
+    parser = optparse.OptionParser('usage%prog -d <netxml directory> -o <output file>')
     parser.add_option('-d', dest='dirpath', type='string', help='specify the directory containing the netxml file')
     parser.add_option('-o', dest='output', type='string', help='specify the output file name')
     (options, args) = parser.parse_args()
